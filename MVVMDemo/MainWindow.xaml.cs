55370213448a921d988f0e521e6c4036f4b76878
franciscoasencio <francisco.asencio@gmail.com>
2021-02-03 19:34:40 -0300

    As per Chapter 6 , implemented  the "Locator" . It works , obiously but the code for the locator is litlle cumbersome...... has always been . Maybe it wil always bee a "Copy-Paste" procedure.  Will move to Chapter 7

--- MVVMDemo/MainWindow.xaml.cs
+++ MVVMDemo/MainWindow.xaml.cs
@@ -3,25 +3,12 @@ using System.Windows;
 
 namespace MVVMDemo
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+ 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        // as in chapter 4 , this is the "Loaded" instruction executed
-        // In chapter 5 This Method is not used. "Load" in constructor, Data Context in Viewmodel Code
-        //private void StudentViewControl_Loaded(object sender, RoutedEventArgs e)
-        //{
-        //    ViewModel.StudentViewModel studentViewModelObject = new ViewModel.StudentViewModel();
-        //    //studentViewModelObject.LoadStudents();
-        //    //StudentViewControl.DataContext = studentViewModelObject;
-        //}
-        ////=================================================================
-
     }
 }
--- MVVMDemo/Views/StudentView.xaml.cs
+++ MVVMDemo/Views/StudentView.xaml.cs
@@ -3,15 +3,12 @@ using System.Windows.Controls;
 
 namespace MVVMDemo.Views
 {
-    /// <summary>
-    /// Interaction logic for StudentView.xaml
-    /// </summary>
     public partial class StudentView : UserControl
     {
         public StudentView()
         {
             InitializeComponent();
-            this.DataContext = new StudentViewModel();
+            //this.DataContext = new StudentViewModel();
         }
     }
 }
